8cd0cf81d2e1874b7c9fdf7ab9750f9873cfb277
Deepak Ganwani <114769596+deepak-ganwani@users.noreply.github.com>
2022-11-19 21:33:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,89 @@
+const grid = document.querySelector('.grid');
+// const columns = document.querySelectorAll('.column');
+let boxes = document.querySelectorAll('.box');
+const list = document.querySelectorAll('li');
+const btn = document.querySelector('#okay');
+const winScreen = document.querySelector('.winner-screen');
+let numOfPlayers=0;
+let count=[...Array(54).fill(0)];
+document.addEventListener('DOMContentLoaded',()=>{for(let i=0;i<54;i++) boxes[i].style=`border-color: ${nextColor}`;})
+
+const addBall = async (i,color) =>{
+
+    boxStack = stackArr[i];
+    // console.log("stack= ",stackArr);
+    boxStack.push(color,i);
+
+    for(let j=0;j<10000;j++)
+    {
+        for(let k=0;k<10000;k++);
+    }
+    // setTimeout(()=>{
+        if(boxStack.size===boxStack.stackTop){
+            // console.log('Executed');
+            boxes[i].innerHTML = ``;            
+            makeQueue(i,boxStack.pop());
+        }
+    // },100);
+    
+    // console.log(boxStack.stackTop);
+}
+
+boxes.forEach((box,i)=>{
+    box.addEventListener('click',async ()=>{
+
+        let color = getColor();
+        if(box.innerHTML==='') 
+        {
+            await addBall(i,color);
+            playerTurn();
+        }
+        else
+        {
+            if(stackArr[i].color==color) 
+            {
+                await addBall(i,color);
+                playerTurn();
+            }
+        } 
+    })
+})
+
+const check = (id)=>{
+    // console.log(id);
+    for(let i=2;i<=6;i++)
+    {   
+        let li = document.getElementById(i);
+        if(i!=id) li.style="background-color: none; color:#ededed;";
+        else li.style="background-color: #ededed; color:black;";
+    }
+}
+
+list.forEach((li)=>{
+    li.addEventListener('click',()=>{
+        if(li.id>=2){
+            numOfPlayers = li.id;
+            check(li.id);
+            document.getElementById('okay').style = "background-color:green";
+        }   
+    })
+})
+
+btn.addEventListener('click',()=>{
+    if(numOfPlayers!==0){
+        document.querySelector('.player-options').style="display:none";
+        grid.style="display:flex";
+
+        for(let i=0;i<numOfPlayers;i++){
+            let player = players.shift();
+            playersUsed.push(player);
+            cll.push(player);
+        }
+    }
+    else{
+        btn.classList.add('wrong');
+        setTimeout(()=>{
+            btn.classList.remove('wrong');
+        },500)
+    }
+})
---
+++ cll.js
@@ -0,0 +1,150 @@
+let turns=0;
+let colors=[];
+let nobc=[...Array(colors.length).fill(0)];
+const winEvent = new Event('win');
+
+class Node{
+    constructor(color){
+        this.color = color;
+        this.next = null;
+    }
+}
+
+class CLL{
+    constructor(){
+        this.count = 0;
+        this.head = undefined;
+    }
+
+    push(color){
+        let node = new Node(color);
+        colors.push(color);
+        let current = this.head;
+        if (current == undefined) {
+            this.head = node;
+            node.next = this.head;
+        } else {
+            while(current.next != this.head){
+                current = current.next;
+            }
+            current.next = node;
+            node.next = this.head;
+        }
+        this.count++;
+    }
+
+    removeAt(index){
+        if (index >= 0 && index < this.count) {
+        //    if (this.isEmpty()) {
+        //        return undefined;
+        //    }
+           let current = this.head
+            if (index == 0) {
+                this.head = current.next;
+            } else if(index == this.count-1) {
+                const previous = this.getcolorAt(index-1);
+                current = previous.next;
+                previous.next = this.head;
+            }else{
+               const previous = this.getcolorAt(index -1);
+               current = previous.next;
+               previous.next = current.next;
+            }
+
+            this.count--;
+            colors.splice(index,1);
+            updateNobc();
+            // return current.color;
+        }
+        // return undefined;
+    }
+
+    getcolorAt(index) {
+        let node = this.head;
+        if (index >= 0 && index < this.count) {
+            for (let i = 0; i < index; i++) {
+                node = node.next;
+            }
+            return node;
+        }
+        return undefined;
+    }
+}
+const cll = new CLL();
+
+const updateNobc = ()=> {
+    nobc=[...Array(colors.length).fill(0)];
+    for(let i=0;i<54;i++){
+        boxStack = stackArr[i];
+        let ind = colors.indexOf(`${boxStack.color}`);
+        nobc[ind]++;
+    }
+
+    //winning condition
+    if(cll.count==1) {
+        setTimeout(()=>{
+        document.dispatchEvent(winEvent);
+        },500);
+    }
+}
+
+// let t = turns%cll.count;
+let currentColor;
+let nextColor='yellow';
+
+// node = cll.head;
+const playerTurn = async ()=>{
+    nextColor=colors[(turns+1)%cll.count];
+    if(cll.count===1) nextColor=colors[0];
+    for(let i=0;i<54;i++) boxes[i].style=`border-color: ${nextColor}`;
+    let node = cll.head;
+    let t = turns%cll.count;
+    while(t--) {
+        node=node.next;
+    }
+    if(cll.count!=1){
+        currentColor=getColor();
+        turns++;
+        // console.log(nextColor)
+        // for(let i=0;i<54;i++) boxes[i].style=`border-color: ${nextColor}`;
+        // Player selects the box
+        // Box operations (Can't select box of other color)
+        // Selected box operations (Changing color of neighboring boxes)
+        // Reevaluate the nobc of every player
+        updateNobc();
+        if(turns>colors.length){
+            let temp=cll.count;
+            for(let i=0;i<temp;i++){
+                // console.log("0 Nobc: "+nobc.indexOf(0));
+                cll.removeAt(nobc.indexOf(0));
+                turns = cll.count+1+colors.indexOf(currentColor);
+            }
+            nextColor=colors[(turns+1)%cll.count];
+        }
+
+        // If (turns<number of players) kn
+        // Else remove players with nobc=0
+        // Remove that particular color from colors array
+        // Call next if lenght not 1
+        // cll.removeAt(0);
+        // playerTurn(node.next);
+    }
+    // console.log(node.color);
+}
+
+document.addEventListener('win',()=>{
+    winScreen.style="display:block;";
+    grid.style = "display:none";
+    winScreen.innerHTML = `<div>Player ${playersUsed.indexOf(colors[0])+1} wins!</div>`;
+    winScreen.innerHTML += `<button id="button"><a href="index.html">Play Again</a></button>`;
+    winScreen.querySelector('div').style =`color: ${colors[0]};`;
+})
+
+const getColor = ()=>{
+    // console.log("getcolor=",colors[turns%cll.count]);
+    return colors[turns%cll.count];
+}
+
+let players = ['yellow','#da012d','#1560bd','#eaeaea','#fc6a03','#00A300'];
+let playersUsed = [];
+// console.log('Working')
